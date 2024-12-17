d569ca8772f2d16fa0407f17c148baf5a2ab2685
Danbi <verisign_90@naver.com>
2024-12-17 21:26:56 +0900

    logo 적용

--- imports/ui/Nav.jsx
+++ imports/ui/Nav.jsx
@@ -86,8 +86,8 @@ export default () => {
             </div>
             <div className="flex shrink-0 items-center">
               <img
-                alt="Your Company"
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                alt="DevConnect"
+                src="/images/logo.png"
                 className="h-8 w-auto"
               />
             </div>
